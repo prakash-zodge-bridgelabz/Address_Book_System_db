683ba8bd64111c555f764f6c018ff1004f99052f
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2024-01-09 17:39:09 +0530

    Ability to create Address Book Service DB

--- Address_Book_System.sql
+++ Address_Book_System.sql
@@ -1,1 +1,6 @@
 -- Welcome to Address Book System
+
+-- Use case 1 : 
+-- Ability to create a Address Book Service DB
+-- - Use SQL Client to create DB and DB Records
+create database address_book_service;
