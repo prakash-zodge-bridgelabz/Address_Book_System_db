78714e8f392e587da78a8fe35b39772cf81db597
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2024-01-10 11:13:52 +0530

    Added contacts to address book table

--- Address_Book_System.sql
+++ Address_Book_System.sql
@@ -12,5 +12,21 @@ show databases; 						-- Use to display databases
 use address_book_service;				-- use to apply conditions on perticular database
 create table address_book(firstName varchar(15),lastName varchar(15),
 address varchar(30),city varchar(15),state varchar(15),zip varchar(15),
-phoneNumber varchar(10),email varchar(30));
+phoneNumber varchar(11),email varchar(30));
 show tables;							-- use to display all tables from existing database
+
+-- Use case 3 : 
+-- Ability to insert new Contacts to Address Book
+insert into address_book values
+('Prakash','Zodge','N-5, CIDCO','Aurangabad',
+'Maharashtra','431003','9876543210','prakash@gmail.com'),
+
+('Pavan','Zore','N-8, Gangapur','Aurangabad',
+'Maharashtra','431007','9988776655','pavan@gmail.com'),
+
+('Mrinal','Keshav','N-1, Pisadevi','Patna',
+'Uttar Pradesh','234123','9998887776','mrinal@gmail.com'),
+
+('Nikhil','Namdev','N-9, Bajrang Chowk','Phulambri',
+'Kannad','565291','8889997653','nikhil@gmail.com');
+select * from address_book; 						-- Use to display all tables
